3a8b8b2d07474c39d964a002f74ea6d5240a33d3
Jascha Ehrenreich <jascha@jaeh.at>
2014-12-21 20:22:01 +0100

    updated blog.js

--- blog.js
+++ blog.js
@@ -1,12 +1,38 @@
 'use strict';
 var express = require('express')
+  , fs      = require('fs')
+  , path    = require('path')
   , router  = express.Router()
   , path    = require('path')
   , log     = require('magic-log')
 ;
 
-router.get('/blog/:postTitle', function (req, res, next) {
-  log('blog postTitle', req.params.postTitle);
+function compileBlogPosts() {
+  
+}
+
+router.get('/blog', function (req, res, next) {
+  //get 10 newest posts
+  
+});
+
+router.get('/blog/:year/:month/:title', function (req, res, next) {
+  var S          = req.app
+    , viewDir    = S.get('dirs').views
+    , title      = req.params.title
+    , year       = req.params.year
+    , month      = req.params.month
+    , blogPath   = path.join(viewDir, 'blog', year, month, title) 
+    , layoutPath = path.join(viewDir, 'post')
+  ;
+
+  res.render(blogPath, {}, function (err, post) {
+    if ( err ) { log.error('error in magic-blog', err); return next();}
+    res.render(layoutPath, {test: 'testing', post: post}, function (err, content) {
+      if ( err ) { log.error('error in magic-blog', err); return next();}
+      res.send(content);
+    });
+  });
 } );
 
 module.exports = router;
