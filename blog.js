d29d9f56dfe59420426e0b51a8ee26e014912edb
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-05 15:20:07 +0100

    various changes, added multiple routes

--- blog.js
+++ blog.js
@@ -1,66 +1,68 @@
 'use strict';
-var express = require('express')
-  , fs      = require('fs')
-  , path    = require('path')
-  , router  = express.Router()
-  , log     = require('magic-log')
+var express  = require('express')
+  , fs       = require('fs')
+  , path     = require('path')
+  , router   = express.Router()
+  , log      = require('magic-log')
+  , blogData = false
+  , cache    = {}
 ;
 
-router.get('/blog/:year/:month/:title', function (req, res, next) {
+router.get('/', function (req, res, next) {
+  console.log('rendering blog list');
+  res.render('blog/list');
+});
+
+router.get('/:year/:month/:slug', function (req, res, next) {
   var S          = req.app
     , viewDir    = S.get('dirs').views
-    , title      = req.params.title
-    , year       = req.params.year
-    , month      = req.params.month
-    , blogPath   = path.join(viewDir, 'blog', year, month, title) 
-    , layoutPath = path.join(viewDir, 'post')
+    , slug      = req.params.slug
+    , y          = req.params.year
+    , m          = req.params.month
+    , blogRoot   = path.join(viewDir, 'blog')
+    , postPath   = path.join(blogRoot, y, m, slug)
+    , layoutPath = path.join(blogRoot, 'post')
   ;
-  if ( isNaN(year) || isNaN(month) ) {
+  if ( isNaN(y) || isNaN(m) ) {
     return next();
   }
-
-  res.render(blogPath, {}, function (err, post) {
+  res.render(postPath, function (err, post) {
     if ( err ) { log.error('error in magic-blog', err); return next();}
     res.render(layoutPath, {post: post}, function (err, content) {
       if ( err ) { log.error('error in magic-blog', err); return next();}
-      res.status(status).send(content);
+      res.status(200).send(content);
     });
   });
 } );
 
-router.get('/blog', function (req, res, next) {
-  log('blog route called');
-  res.send('blogpostlist');
-});
-
-router.get('/blog/:year', function (req, res, next) {
+router.get('/:year', function (req, res, next) {
   var year = req.params.year;
 
   if ( isNaN(year) ) { return next(); }
 
   log('yearly collection called', year);
-  res.status(status).send('year of blog posts');
+  res.status(200).send('year of blog posts');
 });
 
-router.get('/blog/:year/:month', function (req, res, next) {
+router.get('/:year/:month', function (req, res, next) {
   var year = req.params.year
     , month = req.params.month
   ;
   if ( isNaN(year) || isNaN(month) ) { return next(); }
 
   log('monthly collection called', month, year);
-  res.send('month of blog posts');
+  res.status(200).send('month of blog posts');
 });
 
-router.get('/blog/tag/:tag', function (req, res, next) {
+router.get('/tag/:tag', function (req, res, next) {
   var tag = req.params.tag;
   
-  res.status(status).send('tag page', tag);
+  res.status(200).send('tag page', tag);
 });
 
-router.get('/blog/category/:category', function (req, res, next) {
+router.get('/category/:category', function (req, res, next) {
   var category = req.params.category;
-  res.status(status).send('category page', category);
+  res.status(200).send('category page', category);
 });
 
 module.exports = router;
