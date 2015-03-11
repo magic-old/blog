8cfe708f04f92689c9430214b1dbe85e8bae82c3
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-11 21:09:23 +0100

    babelified

--- src/blog.js
+++ src/blog.js
@@ -4,20 +4,30 @@ var express  = require('express')
   , path     = require('path')
   , router   = express.Router()
   , log      = require('magic-log')
+  , jade     = require('jade')
   //~ , blogData = require(path.join(process.cwd(), '.cache', 'blog.json'))
   , cache    = {}
 ;
 
 router.get('/', function (req, res, next) {
   var S        = req.app
-    , viewDir  = S.get('dirs').views
-    , blogRoot = path.join(viewDir, S.get('blogRoot') || 'blog')
+    , dirs     = S.get('dirs')
+    , blogRoot = path.join(dirs.views, S.get('blogRoot') || 'blog')
     , template = path.join(blogRoot, 'list')
-    , hostname = req.hostname
   ;
-  log.success('hostname', hostname);
-  //~ log.success('rendering blog list with blogData', blogData);
   res.render( template );
+});
+
+
+router.get('/tag/:tag', function (req, res, next) {
+  var tag = req.params.tag;
+
+  res.status(200).send('tag page' + tag);
+});
+
+router.get('/category/:category', function (req, res, next) {
+  var category = req.params.category;
+  res.status(200).send('category page' + category);
 });
 
 router.get('/:year/:month/:slug', function (req, res, next) {
@@ -31,6 +41,7 @@ router.get('/:year/:month/:slug', function (req, res, next) {
     , layoutPath = path.join(blogRoot, 'post')
   ;
   if ( isNaN(y) || isNaN(m) ) { return next(); }
+
   res.render(postPath, function (err, post) {
     if ( err ) { log.error('error in magic-blog', err); return next();}
     res.render(layoutPath, {post: post}, function (err, content) {
@@ -57,17 +68,6 @@ router.get('/:year/:month', function (req, res, next) {
 
   log('monthly collection called', month, year);
   res.status(200).send('month of blog posts');
-});
-
-router.get('/tag/:tag', function (req, res, next) {
-  var tag = req.params.tag;
-  
-  res.status(200).send('tag page', tag);
-});
-
-router.get('/category/:category', function (req, res, next) {
-  var category = req.params.category;
-  res.status(200).send('category page', category);
 });
 
 module.exports = router;
