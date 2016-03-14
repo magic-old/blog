4bfb9f4b0cd0f9b5469d0d2a140e48cfee1ba009
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 22:24:33 +0100

    es7ify

--- src/index.js
+++ src/index.js
@@ -1,73 +1,95 @@
-'use strict';
-var express  = require('express')
-  , fs       = require('fs')
-  , path     = require('path')
-  , router   = express.Router()
-  , log      = require('magic-log')
-  , jade     = require('jade')
-  //~ , blogData = require(path.join(process.cwd(), '.cache', 'blog.json'))
-  , cache    = {}
-;
+import { Router } from 'express';
+import { join } from 'path';
+import log from 'magic-server-log';
+import { isNumber } from 'magic-types';
 
-router.get('/', function (req, res, next) {
-  var S        = req.app
-    , dirs     = S.get('dirs')
-    , blogRoot = path.join(dirs.views, S.get('blogRoot') || 'blog')
-    , template = path.join(blogRoot, 'list')
-  ;
-  res.render( template );
-});
+const router = Router();
 
+const noop = () => {};
 
-router.get('/tag/:tag', function (req, res, next) {
-  var tag = req.params.tag;
+router.get(
+  '/',
+  ({ app }, { render }, next = noop) => {
+    const dirs = app.get('dirs');
+    const blogRoot = join(dirs.views, app.get('blogRoot') || 'blog');
+    const template = join(blogRoot, 'list');
 
-  res.status(200).send('tag page' + tag);
-});
-
-router.get('/category/:category', function (req, res, next) {
-  var category = req.params.category;
-  res.status(200).send('category page' + category);
-});
-
-router.get('/:year/:month/:slug', function (req, res, next) {
-  var S          = req.app
-    , viewDir    = S.get('dirs').views
-    , slug       = req.params.slug
-    , y          = req.params.year
-    , m          = req.params.month
-    , blogRoot   = path.join(viewDir, S.get('blogRoot') || 'blog')
-    , postPath   = path.join(blogRoot, y, m, slug)
-    , layoutPath = path.join(blogRoot, 'post')
-  ;
-  if ( isNaN(y) || isNaN(m) ) { return next(); }
-
-  res.render(postPath, function (err, post) {
-    if ( err ) { log.error('error in magic-blog', err); return next();}
-    res.render(layoutPath, {post: post}, function (err, content) {
-      if ( err ) { log.error('error in magic-blog', err); return next();}
-      res.status(200).send(content);
-    });
+    render(template);
   });
-} );
 
-router.get('/:year', function (req, res, next) {
-  var year = req.params.year;
+router.get(
+  '/tag/:tag',
+  ({ params }, res, next = noop) => {
+    res.status(200).send(`tag page ${params.tag}`);
+  });
 
-  if ( isNaN(year) ) { return next(); }
+router.get(
+  '/category/:category',
+  ({ params }, res, next = noop) => {
+    res.status(200).send(`category page ${params.category}`);
+  });
 
-  log('yearly collection called', year);
-  res.status(200).send('year of blog posts');
-});
+router.get(
+  '/:year/:month/:slug',
+  ({ app, params }, res, next = noop) => {
+    const viewDir = app.get('dirs').views;
+    const slug = params.slug;
+    const y = params.year;
+    const m = params.month;
+    const blogRoot = join(viewDir, app.get('blogRoot') || 'blog');
+    const postPath = join(blogRoot, y, m, slug);
+    const layoutPath = join(blogRoot, 'post');
 
-router.get('/:year/:month', function (req, res, next) {
-  var year = req.params.year
-    , month = req.params.month
-  ;
-  if ( isNaN(year) || isNaN(month) ) { return next(); }
+    if (!isNumber(y) || !isNumber(m)) {
+      return next();
+    }
 
-  log('monthly collection called', month, year);
-  res.status(200).send('month of blog posts');
-});
+    res.render(
+      postPath,
+      (err, post) => {
+        if (err) {
+          log.error('error in magic-blog', err);
+          return next();
+        }
 
-module.exports = router;
+        res.render(
+          layoutPath,
+          { post },
+          (err, content) => {
+            if (err) {
+              log.error('error in magic-blog', err);
+              return next();
+            }
+
+            res.status(200).send(content);
+          });
+      });
+  });
+
+router.get(
+  '/:year',
+  ({ params }, res, next = noop) => {
+    const { year } = params;
+
+    if (!isNumber(year)) {
+      return next();
+    }
+
+    log('yearly collection called', year);
+    res.status(200).send(`year of blog posts ${year}`);
+  });
+
+router.get(
+  '/:year/:month',
+  ({ params }, res, next = noop) => {
+    const { year, month } = params;
+
+    if (!isNumber(year) || !isNumber(month)) {
+      return next();
+    }
+
+    log('monthly collection called', month, year);
+    res.status(200).send(`month of blog posts ${month}/${year}`);
+  });
+
+export default router;
