2b914c9726ab0146bead65026b3de49a26c80877
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 22:25:28 +0100

    add dist

---
+++ index.js
@@ -0,0 +1,112 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _express = require('express');
+
+var _path = require('path');
+
+var _magicServerLog = require('magic-server-log');
+
+var _magicServerLog2 = _interopRequireDefault(_magicServerLog);
+
+var _magicTypes = require('magic-types');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = (0, _express.Router)();
+
+var noop = function noop() {};
+
+router.get('/', function (_ref, _ref2) {
+  var app = _ref.app;
+  var render = _ref2.render;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+
+  var dirs = app.get('dirs');
+  var blogRoot = (0, _path.join)(dirs.views, app.get('blogRoot') || 'blog');
+  var template = (0, _path.join)(blogRoot, 'list');
+
+  render(template);
+});
+
+router.get('/tag/:tag', function (_ref3, res) {
+  var params = _ref3.params;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+
+  res.status(200).send('tag page ' + params.tag);
+});
+
+router.get('/category/:category', function (_ref4, res) {
+  var params = _ref4.params;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+
+  res.status(200).send('category page ' + params.category);
+});
+
+router.get('/:year/:month/:slug', function (_ref5, res) {
+  var app = _ref5.app;
+  var params = _ref5.params;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+
+  var viewDir = app.get('dirs').views;
+  var slug = params.slug;
+  var y = params.year;
+  var m = params.month;
+  var blogRoot = (0, _path.join)(viewDir, app.get('blogRoot') || 'blog');
+  var postPath = (0, _path.join)(blogRoot, y, m, slug);
+  var layoutPath = (0, _path.join)(blogRoot, 'post');
+
+  if (!(0, _magicTypes.isNumber)(y) || !(0, _magicTypes.isNumber)(m)) {
+    return next();
+  }
+
+  res.render(postPath, function (err, post) {
+    if (err) {
+      _magicServerLog2.default.error('error in magic-blog', err);
+      return next();
+    }
+
+    res.render(layoutPath, { post: post }, function (err, content) {
+      if (err) {
+        _magicServerLog2.default.error('error in magic-blog', err);
+        return next();
+      }
+
+      res.status(200).send(content);
+    });
+  });
+});
+
+router.get('/:year', function (_ref6, res) {
+  var params = _ref6.params;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+  var year = params.year;
+
+
+  if (!(0, _magicTypes.isNumber)(year)) {
+    return next();
+  }
+
+  (0, _magicServerLog2.default)('yearly collection called', year);
+  res.status(200).send('year of blog posts ' + year);
+});
+
+router.get('/:year/:month', function (_ref7, res) {
+  var params = _ref7.params;
+  var next = arguments.length <= 2 || arguments[2] === undefined ? noop : arguments[2];
+  var year = params.year;
+  var month = params.month;
+
+
+  if (!(0, _magicTypes.isNumber)(year) || !(0, _magicTypes.isNumber)(month)) {
+    return next();
+  }
+
+  (0, _magicServerLog2.default)('monthly collection called', month, year);
+  res.status(200).send('month of blog posts ' + month + '/' + year);
+});
+
+exports.default = router;
